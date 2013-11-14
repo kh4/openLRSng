2feb362196cc0bfdab36113b7d4110d1f4ff70e9
Martin <martin.collberg@gmail.com>
2013-11-14 04:12:20 +0100

    style after merge

--- binding.h
+++ binding.h
@@ -88,27 +88,30 @@ static uint8_t default_hop_list[] = {DEFAULT_HOPLIST};
 
 #define MAXHOPS 24
 
-struct bind_data {
-  uint8_t version;
-  uint32_t serial_baudrate;
-  uint8_t serial_downlink; // 0-63 max byte count for serial downlink
-  uint32_t rf_frequency;
-  uint32_t rf_magic;
-  uint8_t rf_power;
-  uint8_t rf_channel_spacing;
-  uint8_t hopchannel[MAXHOPS];
-  uint8_t modem_params;
-  uint8_t flags;
+struct bind_data
+{
+    uint8_t version;
+    uint32_t serial_baudrate;
+    uint8_t serial_downlink; // 0-63 max byte count for serial downlink
+    uint32_t rf_frequency;
+    uint32_t rf_magic;
+    uint8_t rf_power;
+    uint8_t rf_channel_spacing;
+    uint8_t hopchannel[MAXHOPS];
+    uint8_t modem_params;
+    uint8_t flags;
 } bind_data;
 
 
-struct rfm22_modem_regs {
-  uint32_t bps;
-  uint8_t  r_1c, r_1d, r_1e, r_20, r_21, r_22, r_23, r_24, r_25, r_2a, r_6e, r_6f, r_70, r_71, r_72;
-} modem_params[] = {
-  { 4800, 0x1a, 0x40, 0x0a, 0xa1, 0x20, 0x4e, 0xa5, 0x00, 0x1b, 0x1e, 0x27, 0x52, 0x2c, 0x23, 0x30 }, // 50000 0x00
-  { 9600, 0x05, 0x40, 0x0a, 0xa1, 0x20, 0x4e, 0xa5, 0x00, 0x20, 0x24, 0x4e, 0xa5, 0x2c, 0x23, 0x30 }, // 25000 0x00
-  { 19200, 0x06, 0x40, 0x0a, 0xd0, 0x00, 0x9d, 0x49, 0x00, 0x7b, 0x28, 0x9d, 0x49, 0x2c, 0x23, 0x30 } // 25000 0x01
+struct rfm22_modem_regs
+{
+    uint32_t bps;
+    uint8_t  r_1c, r_1d, r_1e, r_20, r_21, r_22, r_23, r_24, r_25, r_2a, r_6e, r_6f, r_70, r_71, r_72;
+} modem_params[] =
+{
+    { 4800, 0x1a, 0x40, 0x0a, 0xa1, 0x20, 0x4e, 0xa5, 0x00, 0x1b, 0x1e, 0x27, 0x52, 0x2c, 0x23, 0x30 }, // 50000 0x00
+    { 9600, 0x05, 0x40, 0x0a, 0xa1, 0x20, 0x4e, 0xa5, 0x00, 0x20, 0x24, 0x4e, 0xa5, 0x2c, 0x23, 0x30 }, // 25000 0x00
+    { 19200, 0x06, 0x40, 0x0a, 0xd0, 0x00, 0x9d, 0x49, 0x00, 0x7b, 0x28, 0x9d, 0x49, 0x2c, 0x23, 0x30 } // 25000 0x01
 };
 
 #define DATARATE_COUNT (sizeof(modem_params)/sizeof(modem_params[0]))
@@ -122,90 +125,103 @@ void fatalBlink(uint8_t blinks);
 // Save EEPROM by writing just changed data
 void myEEPROMwrite(int16_t addr, uint8_t data)
 {
-  uint8_t retries = 5;
-  while ((--retries) && (data != EEPROM.read(addr))) {
-    EEPROM.write(addr, data);
-  }
-  if (!retries) {
-    fatalBlink(2);
-  }
+    uint8_t retries = 5;
+    while ((--retries) && (data != EEPROM.read(addr)))
+    {
+        EEPROM.write(addr, data);
+    }
+    if (!retries)
+    {
+        fatalBlink(2);
+    }
 }
 
 int16_t bindReadEeprom()
 {
-  uint32_t temp = 0;
-  for (uint8_t i = 0; i < 4; i++) {
-    temp = (temp << 8) + EEPROM.read(EEPROM_OFFSET + i);
-  }
-  if (temp != BIND_MAGIC) {
-    return 0;
-  }
+    uint32_t temp = 0;
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        temp = (temp << 8) + EEPROM.read(EEPROM_OFFSET + i);
+    }
+    if (temp != BIND_MAGIC)
+    {
+        return 0;
+    }
 
-  for (uint8_t i = 0; i < sizeof(bind_data); i++) {
-    *((uint8_t*)&bind_data + i) = EEPROM.read(EEPROM_OFFSET + 4 + i);
-  }
+    for (uint8_t i = 0; i < sizeof(bind_data); i++)
+    {
+        *((uint8_t*)&bind_data + i) = EEPROM.read(EEPROM_OFFSET + 4 + i);
+    }
 
-  if (bind_data.version != BINDING_VERSION) {
-    return 0;
-  }
+    if (bind_data.version != BINDING_VERSION)
+    {
+        return 0;
+    }
 
-  return 1;
+    return 1;
 }
 
 void bindWriteEeprom(void)
 {
-  for (uint8_t i = 0; i < 4; i++) {
-    myEEPROMwrite(EEPROM_OFFSET + i, (BIND_MAGIC >> ((3 - i) * 8)) & 0xff);
-  }
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        myEEPROMwrite(EEPROM_OFFSET + i, (BIND_MAGIC >> ((3 - i) * 8)) & 0xff);
+    }
 
-  for (uint8_t i = 0; i < sizeof(bind_data); i++) {
-    myEEPROMwrite(EEPROM_OFFSET + 4 + i, *((uint8_t*)&bind_data + i));
-  }
+    for (uint8_t i = 0; i < sizeof(bind_data); i++)
+    {
+        myEEPROMwrite(EEPROM_OFFSET + 4 + i, *((uint8_t*)&bind_data + i));
+    }
 }
 
 void bindInitDefaults(void)
 {
-  bind_data.version = BINDING_VERSION;
-  bind_data.serial_baudrate = DEFAULT_BAUDRATE;
-  bind_data.serial_downlink = DEFAULT_SERIAL_DOWNLINK;
-  bind_data.rf_power = DEFAULT_RF_POWER;
-  bind_data.rf_frequency = DEFAULT_CARRIER_FREQUENCY;
-  bind_data.rf_channel_spacing = DEFAULT_CHANNEL_SPACING;
+    bind_data.version = BINDING_VERSION;
+    bind_data.serial_baudrate = DEFAULT_BAUDRATE;
+    bind_data.serial_downlink = DEFAULT_SERIAL_DOWNLINK;
+    bind_data.rf_power = DEFAULT_RF_POWER;
+    bind_data.rf_frequency = DEFAULT_CARRIER_FREQUENCY;
+    bind_data.rf_channel_spacing = DEFAULT_CHANNEL_SPACING;
 
-  bind_data.rf_magic = DEFAULT_RF_MAGIC;
+    bind_data.rf_magic = DEFAULT_RF_MAGIC;
 
-  for (uint8_t c = 0; c < MAXHOPS; c++) {
-    bind_data.hopchannel[c] = (c < sizeof(default_hop_list)) ? default_hop_list[c] : 0;
-  }
+    for (uint8_t c = 0; c < MAXHOPS; c++)
+    {
+        bind_data.hopchannel[c] = (c < sizeof(default_hop_list)) ? default_hop_list[c] : 0;
+    }
 
-  bind_data.modem_params = DEFAULT_DATARATE;
-  bind_data.flags = DEFAULT_FLAGS;
+    bind_data.modem_params = DEFAULT_DATARATE;
+    bind_data.flags = DEFAULT_FLAGS;
 }
 
 void bindRandomize(void)
 {
-  uint8_t c;
+    uint8_t c;
 
-  randomSeed(micros());
+    randomSeed(micros());
 
-  bind_data.rf_magic = 0;
-  for (c = 0; c < 4; c++) {
-    bind_data.rf_magic = (bind_data.rf_magic << 8) + random(255);
-  }
-
-  for (c = 0; (c < MAXHOPS) && (bind_data.hopchannel[c] != 0); c++) {
-again:
-    uint8_t ch = random(50) + 1;
-
-    // don't allow same channel twice
-    for (uint8_t i = 0; i < c; i++) {
-      if (bind_data.hopchannel[i] == ch) {
-        goto again;
-      }
+    bind_data.rf_magic = 0;
+    for (c = 0; c < 4; c++)
+    {
+        bind_data.rf_magic = (bind_data.rf_magic << 8) + random(255);
     }
 
-    bind_data.hopchannel[c] = ch;
-  }
+    for (c = 0; (c < MAXHOPS) && (bind_data.hopchannel[c] != 0); c++)
+    {
+again:
+        uint8_t ch = random(50) + 1;
+
+        // don't allow same channel twice
+        for (uint8_t i = 0; i < c; i++)
+        {
+            if (bind_data.hopchannel[i] == ch)
+            {
+                goto again;
+            }
+        }
+
+        bind_data.hopchannel[c] = ch;
+    }
 }
 
 #define FAILSAFE_NOPPM    0x01
@@ -216,16 +232,17 @@ again:
 
 #define FAILSAFE_TIME(rxc) (((uint32_t)rxc.failsafe_delay) * 100000UL)
 
-struct RX_config {
-  uint8_t  rx_type; // RX type fillled in by RX, do not change
-  uint8_t  pinMapping[13];
-  uint8_t  flags;
-  uint8_t  RSSIpwm;
-  uint32_t beacon_frequency;
-  uint16_t beacon_deadtime;
-  uint8_t  beacon_interval;
-  uint16_t minsync;
-  uint8_t  failsafe_delay;
+struct RX_config
+{
+    uint8_t  rx_type; // RX type fillled in by RX, do not change
+    uint8_t  pinMapping[13];
+    uint8_t  flags;
+    uint8_t  RSSIpwm;
+    uint32_t beacon_frequency;
+    uint16_t beacon_deadtime;
+    uint8_t  beacon_interval;
+    uint16_t minsync;
+    uint8_t  failsafe_delay;
 } rx_config;
 
 #ifndef COMPILE_TX
@@ -233,66 +250,75 @@ struct RX_config {
 
 void rxInitDefaults()
 {
-  uint8_t i;
+    uint8_t i;
 #if (BOARD_TYPE == 3)
-  rx_config.rx_type = RX_FLYTRON8CH;
-  rx_config.pinMapping[0] = PINMAP_RSSI; // the CH0 on 8ch RX
-  for (i = 1; i < 9; i++) {
-    rx_config.pinMapping[i] = i - 1; // default to PWM out
-  }
-  rx_config.pinMapping[9] = PINMAP_ANALOG;
-  rx_config.pinMapping[10] = PINMAP_ANALOG;
-  rx_config.pinMapping[11] = PINMAP_RXD;
-  rx_config.pinMapping[12] = PINMAP_TXD;
+    rx_config.rx_type = RX_FLYTRON8CH;
+    rx_config.pinMapping[0] = PINMAP_RSSI; // the CH0 on 8ch RX
+    for (i = 1; i < 9; i++)
+    {
+        rx_config.pinMapping[i] = i - 1; // default to PWM out
+    }
+    rx_config.pinMapping[9] = PINMAP_ANALOG;
+    rx_config.pinMapping[10] = PINMAP_ANALOG;
+    rx_config.pinMapping[11] = PINMAP_RXD;
+    rx_config.pinMapping[12] = PINMAP_TXD;
 #elif (BOARD_TYPE == 5)
-  rx_config.rx_type = RX_OLRSNG4CH;
-  for (i = 0; i < 4; i++) {
-    rx_config.pinMapping[i] = i; // default to PWM out
-  }
-  rx_config.pinMapping[4] = PINMAP_RXD;
-  rx_config.pinMapping[5] = PINMAP_TXD;
+    rx_config.rx_type = RX_OLRSNG4CH;
+    for (i = 0; i < 4; i++)
+    {
+        rx_config.pinMapping[i] = i; // default to PWM out
+    }
+    rx_config.pinMapping[4] = PINMAP_RXD;
+    rx_config.pinMapping[5] = PINMAP_TXD;
 #else
 #error INVALID RX BOARD
 #endif
 
-  rx_config.flags = ALWAYS_BIND;
-  rx_config.RSSIpwm = 255; // off
-  rx_config.failsafe_delay = 10; //1s
-  rx_config.beacon_frequency = DEFAULT_BEACON_FREQUENCY;
-  rx_config.beacon_deadtime = DEFAULT_BEACON_DEADTIME;
-  rx_config.beacon_interval = DEFAULT_BEACON_INTERVAL;
-  rx_config.minsync = 3000;
+    rx_config.flags = ALWAYS_BIND;
+    rx_config.RSSIpwm = 255; // off
+    rx_config.failsafe_delay = 10; //1s
+    rx_config.beacon_frequency = DEFAULT_BEACON_FREQUENCY;
+    rx_config.beacon_deadtime = DEFAULT_BEACON_DEADTIME;
+    rx_config.beacon_interval = DEFAULT_BEACON_INTERVAL;
+    rx_config.minsync = 3000;
 }
 
 void rxWriteEeprom()
 {
-  for (uint8_t i = 0; i < 4; i++) {
-    myEEPROMwrite(EEPROM_RX_OFFSET + i, (BIND_MAGIC >> ((3 - i) * 8)) & 0xff);
-  }
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        myEEPROMwrite(EEPROM_RX_OFFSET + i, (BIND_MAGIC >> ((3 - i) * 8)) & 0xff);
+    }
 
-  for (uint8_t i = 0; i < sizeof(rx_config); i++) {
-    myEEPROMwrite(EEPROM_RX_OFFSET + 4 + i, *((uint8_t*)&rx_config + i));
-  }
+    for (uint8_t i = 0; i < sizeof(rx_config); i++)
+    {
+        myEEPROMwrite(EEPROM_RX_OFFSET + 4 + i, *((uint8_t*)&rx_config + i));
+    }
 }
 
 void rxReadEeprom()
 {
-  uint32_t temp = 0;
+    uint32_t temp = 0;
 
-  for (uint8_t i = 0; i < 4; i++) {
-    temp = (temp << 8) + EEPROM.read(EEPROM_RX_OFFSET + i);
-  }
-
-  if (temp != BIND_MAGIC) {
-    Serial.println("RXconf reinit");
-    rxInitDefaults();
-    rxWriteEeprom();
-  } else {
-    for (uint8_t i = 0; i < sizeof(rx_config); i++) {
-      *((uint8_t*)&rx_config + i) = EEPROM.read(EEPROM_RX_OFFSET + 4 + i);
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        temp = (temp << 8) + EEPROM.read(EEPROM_RX_OFFSET + i);
     }
-    Serial.println("RXconf loaded");
-  }
+
+    if (temp != BIND_MAGIC)
+    {
+        Serial.println("RXconf reinit");
+        rxInitDefaults();
+        rxWriteEeprom();
+    }
+    else
+    {
+        for (uint8_t i = 0; i < sizeof(rx_config); i++)
+        {
+            *((uint8_t*)&rx_config + i) = EEPROM.read(EEPROM_RX_OFFSET + 4 + i);
+        }
+        Serial.println("RXconf loaded");
+    }
 }
 
 #endif
